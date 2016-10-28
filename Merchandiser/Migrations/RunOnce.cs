39f1b108e56db623e0a984ff68fe3ff502982112
joel1618 <joel1618@gmail.com>
2016-10-28 13:11:42 -0500

    Add db script

---
+++ Merchandiser/Migrations/RunOnce.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Merchandiser.Migrations
+{
+    public class RunOnce
+    {
+        /*
+         INSERT INTO dbo.SurveyCustomerLocationProductQuestion
+        ( Id ,
+          CompanyId ,
+          SurveyId ,
+          CustomerId ,
+          LocationId ,
+          ProductId ,
+          QuestionId ,
+          RowOrder ,
+          Created ,
+          CreatedBy
+        )
+        SELECT NEWID() AS 'Id', scl.CompanyId, scl.SurveyId, 
+        scl.CustomerId, scl.LocationId, 
+        spq.ProductId, spq.QuestionId, 0, GETDATE() 'Created', Survey.CreatedBy 
+
+        FROM dbo.SurveyCustomerLocation scl
+        LEFT JOIN dbo.Survey
+        ON scl.SurveyId = survey.Id
+        LEFT JOIN dbo.SurveyProductQuestion spq
+        ON spq.SurveyId = survey.Id
+         * */
+    }
+}
